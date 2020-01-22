03235c54b544594002f2c539cd1098d3d237a3c0
Pedro Paes Siniscalchi <ppaes94_ps@hotmail.com>
2020-01-22 01:43:50 -0300

    Python Game

--- Forca.py
+++ Forca.py
@@ -1,8 +1,25 @@
-
 def jogar():
     print("***************************")
     print("Bem Vindo ao jogo de Forca!")
     print("***************************")
+
+    palavra_secreta = "Banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual a letra?")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Econtrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+        print("Jogando...")
 
     print("Fim de Jogo!")
 
