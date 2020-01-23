3fdc85e7d2b01f718ef0e8a6e6b52757172a0d17
Pedro Paes Siniscalchi <ppaes94_ps@hotmail.com>
2020-01-23 19:55:17 -0300

    Python Game

--- Forca.py
+++ Forca.py
@@ -1,12 +1,22 @@
+import random
+
 def jogar():
     print("***************************")
     print("Bem Vindo ao jogo de Forca!")
     print("***************************")
 
-    palavra_secreta = "Banana".upper()
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+    arquivo.close()
+
+    numero = random.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
     letras_acertadas = ["_" for letra in palavra_secreta]
-
-
 
     enforcou = False
     acertou = False
